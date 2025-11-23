6f12355055f6d47935119564fab621bd49adec37
SarwechAbro <sarwechabro@gmail.com>
2025-11-23 14:41:17 +0500

    add STATICFILES_DIRS to settings for static file management

--- schoolapp/schoolapp/settings.py
+++ schoolapp/schoolapp/settings.py
@@ -92,8 +92,6 @@ DATABASES = {
 
 
 # Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -121,6 +119,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
