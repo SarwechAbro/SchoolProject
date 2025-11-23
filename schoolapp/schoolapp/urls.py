46cb9961351e61de566456a4eae507aeb4c1732e
SarwechAbro <sarwechabro@gmail.com>
2025-11-23 15:00:52 +0500

    comments added for better readability

--- schoolapp/schoolapp/urls.py
+++ schoolapp/schoolapp/urls.py
@@ -9,8 +9,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('django.contrib.auth.urls')),
     path('', include('myapp.urls')),
+    # JWT Token Authentication URLs
     path('token/get/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),   
-    
+    # for media files
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
